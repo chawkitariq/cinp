0f3b259fcab9f46a7ad44c9c9896d3ba49ad076b
chawkitariq <chawkitariq@icloud.com>
2026-06-11 18:14:08 +0200

    feat(packages/api): setup assessment module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { ProblemModule } from './problem/problem.module';
+import { AssessmentModule } from './assessment/assessment.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, ProblemModule],
+  imports: [DatabaseModule, UserModule, ProblemModule, AssessmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
