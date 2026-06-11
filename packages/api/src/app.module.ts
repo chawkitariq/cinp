ba38877772383b6bd64eb8d898b2b1ca333fa666
chawkitariq <chawkitariq@icloud.com>
2026-06-11 18:13:42 +0200

    feat(packages/api): setup problem module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { ProblemModule } from './problem/problem.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, ProblemModule],
   controllers: [AppController],
   providers: [AppService],
 })
