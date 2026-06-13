62bd00e8c33d42c17a45ae281a17fde227ad4206
chawkitariq <chawkitariq@icloud.com>
2026-06-13 16:24:27 +0200

    test:(packages/web): setup jest/react testing library"

---
+++ packages/web/jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+export default createJestConfig(customJestConfig);
